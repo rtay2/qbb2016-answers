4e703e132e35a421c7e3cc42616f77eb9a2bf303
Rebecca Tay <rtay2@jhu.edu>
2016-08-30 20:41:26 -0400

    May not be quite right with the default vs. not option

--- day2-homework/d2_Identifier_Mapping.py
+++ day2-homework/d2_Identifier_Mapping.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+'''Takes three arguments(full output file from mapping file, ctab file in other directory to be matched against the dictionary, option as either "Ignore" or "Default" on whether ctab lines with no fly id match should be ignored or the line read with a * in place of the uni id). 
+'''
+
 import sys
 
 mapping_file = open("full_output.txt")
@@ -13,11 +16,21 @@ for line in mapping_file.readlines():
 
 our_data_ctab = open(sys.argv[1])
 
+option = sys.argv[2]
+
 for line in our_data_ctab.readlines():
     line=line.strip()
     cols = line.split("\t")
     ctab_FBg = cols[8]
     if ctab_FBg in dict1:
         print line+'\t'+dict1[ctab_FBg]
+    else:
+        if option == "Ignore":
+            continue
+        elif option == "Default":
+            print line+"\t"+"*"
+            
     
+            
+            
         
