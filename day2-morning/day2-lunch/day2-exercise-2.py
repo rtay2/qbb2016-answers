7caebbe20767fa3962138894dddb1f0f3761207b
Rebecca Tay <rtay2@jhu.edu>
2016-08-31 00:15:51 -0400

    Changing XM to NM

--- day2-morning/day2-lunch/day2-exercise-2.py
+++ day2-morning/day2-lunch/day2-exercise-2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-f = open("mappedreads.sam")
+import sys
+f = open(sys.argv[1])
 
 count = 0
 for line in f.readlines():
-    if "XM:i:0" in line:
+    if "NM:i:0" in line:
         count += 1 
 print count 
