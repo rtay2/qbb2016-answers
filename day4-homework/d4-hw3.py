8777cede07db882ee15ca25014f7a8f4ec496733
Rebecca Tay <rtay2@jhu.edu>
2016-09-02 09:53:15 -0400

    Modified exercise 3

--- day4-homework/d4-hw3.py
+++ day4-homework/d4-hw3.py
@@ -4,6 +4,7 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+plt.style.use('ggplot')
 
 df = pd.read_table(sys.argv[1])
 df2 = pd.read_table(sys.argv[2])
@@ -11,14 +12,14 @@ df2 = pd.read_table(sys.argv[2])
 expr_R = df["FPKM"]
 expr_G = df2["FPKM"]
 
-log_R = np.log2(expr_R)
-log_G = np.log2(expr_G)
+log_R = np.log2(expr_R + 1)
+log_G = np.log2(expr_G + 1)
 
 the_m = log_R - log_G
 the_a = 0.5 * (log_R + log_G)
 
 plt.figure()
-plt.scatter(the_m + 1, the_a + 1, alpha = 0.2, color ='b')
+plt.scatter(the_a, the_m, alpha = 0.1, color ='b')
 plt.xlabel("A")
 plt.ylabel("M")
 plt.title("SRR072893 v SRR072915")
