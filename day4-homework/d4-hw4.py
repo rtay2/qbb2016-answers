e4a7c1219c64974be42252c042fa54eb79885e5f
Rebecca Tay <rtay2@jhu.edu>
2016-09-02 15:43:20 -0400

    Fixing density plot

--- day4-homework/d4-hw4.py
+++ day4-homework/d4-hw4.py
@@ -9,16 +9,20 @@ from scipy.stats import gaussian_kde
 
 df = pd.read_table(sys.argv[1])
 
-for_dens = df["FPKM"]
-density = gaussian_kde(for_dens)
-data = np.linspace(-50,50,300)
+for_dens = df["FPKM"] + 1
+w_log = np.log10(for_dens)
+
+density = gaussian_kde(w_log)
+data = np.linspace(-1,4, 200)
 
 plt.figure()
-plt.plot(data, density(data), linewidth = 2)
+plt.plot(data, density(data), linewidth = 1)
 plt.ylabel("Density")
+plt.xlabel("FPKM")
 plt.title("Kernel Density for SRR072893")
-plt.tick_params(axis='x',top='off',bottom='off')
-plt.tick_params(axis='y',left='off',right='off')
+plt.tick_params(axis='x',top='off')
+plt.tick_params(axis='y',left='off')
 plt.savefig("KDE_plot.png")
 plt.close()
+
 
